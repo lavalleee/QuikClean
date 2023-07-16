cdfcbb2f55ac84d4a35ff737502e40f24246d409
Michael Lavallee <michaelalavallee121@gmail.com>
2023-07-16 15:16:33 -0400

    Made QuikClean more quick!

--- QuikClean/cleaner.sh
+++ QuikClean/cleaner.sh
@@ -5,18 +5,12 @@ echo "Thank you for using my tool."
 echo "Clearing PageCache, dentries and inodes..."
 echo ""
 
-sleep 2 
-
 sync; echo 3 > /proc/sys/vm/drop_caches
 
 echo "Cleared!"
 
-sleep 1
-
 echo ""
 echo "Clearing Swap Space..."
-
-sleep 2 
 
 swapoff -a && swapon -a
 
